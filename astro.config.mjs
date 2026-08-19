586f041e0fcce4710db6f0741cfbe6e2d3ca93fc
Arcade Wise <l3gacy.b3ta@gmail.com>
2026-08-19 12:29:40 -0400

    oops baseurl

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // GitHub Pages project site: https://arcadewise.github.io/althistdocs
 // If the repo gets a custom domain later, set base to '/' and update site.
 export default defineConfig({
-  site: 'https://arcadewise.github.io',
-  base: '/althistdocs',
+  site: 'https://arcades.agency',
+  base: '/alternate-history',
 });
